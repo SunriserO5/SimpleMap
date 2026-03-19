2106e099c81d87baffff8f2dfbc72b3e0f1ecff7
SunriserO5 <edwardliu05@icloud.com>
2026-03-19 20:38:07 +0800

    基本数据结构

---
+++ dataStructure.h
@@ -0,0 +1,53 @@
+#ifndef DATASTRUCTURE_H
+#define DATASTRUCTURE_H
+
+
+struct mapNode
+{
+    int nodeID;
+    int x_coordinate;
+    int y_coordinate;
+    int numOfNeighbour;
+    int* Neighbour;
+    int* distanceToNeighbour;
+    mapNode(int id=-1,int x=-1,int y=-1,int neighbours=0){
+        nodeID=id;
+        x_coordinate=x;
+        y_coordinate=y;
+        numOfNeighbour=neighbours;
+        Neighbour=new int[neighbours];
+        distanceToNeighbour=new int[neighbours];
+    }
+};
+
+struct cacheNode{
+    int beginNodeID;
+    int endNodeID;
+    int distance;
+    int* path;
+    cacheNode(int begin=-1,int end=-1,int dis=-1,int* p=nullptr){
+        beginNodeID=begin;
+        endNodeID=end;
+        distance=dis;
+        path=p;
+}
+};
+
+class mapGraphBase
+{
+    int numOfNodes;
+    int numOfEdges;
+    mapNode* mainGraph;
+    int cacheDistance[100];
+public:
+    virtual int getNumOfNodes()=0;
+    virtual int getNumOfEdges()=0;
+    virtual mapNode* getMainGraph()=0;
+    virtual int* getCoordinate(int nodeID)=0;
+    mapGraphBase(int nodes=0,int edges=0,mapNode* graph=nullptr);
+    ~mapGraphBase();
+    virtual int getPath(int nodeID1,int nodeID2)=0;
+
+};
+
+#endif // DATASTRUCTURE_H
